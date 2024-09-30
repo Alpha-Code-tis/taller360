4f9dc0f6e3f318a2053e834d2f962df692513468
LizbethHY <82045375+LizbethHY@users.noreply.github.com>
2024-09-30 14:45:03 -0400

    validar num grupos

--- backend/app/Http/Controllers/GrupoController.php
+++ backend/app/Http/Controllers/GrupoController.php
@@ -11,20 +11,11 @@ class GrupoController extends Controller
     // Mostrar todos los grupos
     public function index()
     {
-        // Obtener todos los grupos
         $grupos = Grupo::all();
-
-        // Obtener los grupos que ya están asignados a docentes
-        $gruposOcupados = Docente::pluck('id_grupo')->toArray();
-
-        // Filtrar los grupos que no están en uso
-        $gruposDisponibles = $grupos->filter(function($grupo) use ($gruposOcupados) {
-            return !in_array($grupo->id_grupo, $gruposOcupados);
-        });
-        return response()->json($gruposDisponibles->values()->all());
+        return response()->json($grupos);
     }
-    // Mostrar un grupo en específico
 
+    // Mostrar un grupo en específico
     public function show($id)
     {
         $grupo = Grupo::find($id);
@@ -39,28 +30,29 @@ class GrupoController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'nro_grupo' => 'nullable|string|max:10',
+            'nro_grupo' => 'nullable|numeric|max:10',  // Changed to numeric
         ]);
-
+    
         $grupo = Grupo::create($validatedData);
         return response()->json($grupo, 201);
     }
-
+    
     public function update(Request $request, $id)
     {
         $grupo = Grupo::find($id);
-
+    
         if (!$grupo) {
             return response()->json(['error' => 'Grupo no encontrado'], 404);
         }
-
+    
         $validatedData = $request->validate([
-            'nro_grupo' => 'nullable|string|max:10',
+            'nro_grupo' => 'nullable|numeric|max:10',  // Changed to numeric
         ]);
-
+    
         $grupo->update($validatedData);
         return response()->json($grupo);
     }
+    
 
     public function destroy($id)
     {
